ae62ef54043b5787c60238a7414616bbc687f8eb
feiandxs <yiziyint@gmail.com>
2024-06-20 03:33:22 +0000

    chore: Add main layout, loading component, and update page layout

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,16 @@
-export default function Home() {
+function delay(ms: number) {
+  return new Promise((resolve) => { setTimeout(resolve, ms); });
+}
+
+const MainPage = async () => {
+  await delay(3000);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        nextjs scaffold
+        main page
       </div>
     </main>
   );
-}
+};
+
+export default MainPage;
---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+import { siteInfo } from '@/constatns/site-info';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: siteInfo.title,
+  description: siteInfo.description,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
