4c7dbfd35126715385caf01d0ac87181046f90ae
feiandxs <yiziyint@gmail.com>
2024-08-18 00:02:47 +0800

    chore: Update main page for nextjs scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const MainPage = async () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        main page
+        main page for nextjs scaffold
       </div>
     </main>
   );
