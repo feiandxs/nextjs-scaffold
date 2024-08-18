7820c77cba15079eedd1df75f248c6054db0fe4f
feiandxs <yiziyint@gmail.com>
2024-08-18 23:14:14 +0800

    chore: Update eslintignore and package.json, add dark mode toggle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 
 import { siteInfo } from '@/constants/site-info';
 
+import { ModeToggle } from '@/components/common/darkmode-toggle';
+
 function delay(ms: number) {
   return new Promise((resolve) => { setTimeout(resolve, ms); });
 }
@@ -20,6 +22,7 @@ const MainPage = async () => {
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         main page for nextjs scaffold
       </div>
+      <ModeToggle />
     </main>
   );
 };
