f15fe2bd06098d4ad98ccc2669a10c134e640799
feiandxs <yiziyint@gmail.com>
2025-04-05 11:07:51 +0800

    refactor: 在 FrontPage 组件中添加渲染日志

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,7 +1,14 @@
-const FrontPage = () => (
-  <div>
-    <h1>Welcome to the Front Page</h1>
-    <p>This is the front page of your application.</p>
-  </div>
+const FrontPage = () => {
+  console.log('FrontPage component rendered');
+  return (
+    <div>
+      (
+      <div>
+        <h1>Welcome to the Front Page</h1>
+        <p>This is the front page of your application.</p>
+      </div>
+      )
+    </div>
   )
+}
 export default FrontPage;
